631e334feb551000a772894c74c4197864c4b6b6
Chougle-saud <saudchougle.sc@gmail.com>
2019-03-11 23:35:47 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,22 @@
+                            // Resources
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs= require("ejs");
+const path = require("path");
+
+                           // Initialization
+const app = express();
+const port = process.env.PORT || 3001;
+app.listen(port,()=>{console.log(`server is listening at port:port`)});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,"./public/")))
+app.set("view engine","ejs");
+
+                           // Routes
+app.get("/",(req,res)=>{
+    res.render("homepage")
+});
+
+
+
